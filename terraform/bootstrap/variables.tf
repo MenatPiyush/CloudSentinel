a0cea89b2d1e5c939412f1c03a5a03f0630148c3
Piyush Menat <menatp97@Piyushs-MacBook-Air.local>
2026-01-31 21:27:22 -0400

    add vpc

--- terraform/bootstrap/variables.tf
+++ terraform/bootstrap/variables.tf
@@ -1,12 +1,9 @@
-variable "region" { 
-    type = string 
-    default = "us-east-2" 
-}
-variable "state_bucket_name" { 
+variable "region" {
     type = string
-    default = "loudsentinel-terraform-state-8fb8bf92"
 }
-variable "lock_table_name"{ 
-    type = string 
-    default = "terraform-locks-74c26fe8" 
+variable "state_bucket_name" {
+    type = string
+}
+variable "lock_table_name" {
+    type = string
 }
