4b60f30664df01613b31de739c41abd2942a2235
Piyush Menat <menatp97@Piyushs-MacBook-Air.local>
2026-01-31 22:24:08 -0400

    add eks module

--- terraform/workload-prod/main.tf
+++ terraform/workload-prod/main.tf
@@ -8,3 +8,12 @@ module "vpc" {
   cidr = var.vpc_cidr
   azs = var.azs
 }
+
+module "eks" {
+  source              = "./modules/eks"
+  name                = var.name
+  vpc_id              = module.vpc.vpc_id
+  private_subnet_ids   = module.vpc.private_subnet_ids
+  cluster_version      = var.eks_version
+  node_instance_types  = var.node_instance_types
+}
